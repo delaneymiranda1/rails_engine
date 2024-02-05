c96645d8060aaa481506f57449b1edab8119808d
Nathan Lambertson <lambertones@gmail.com>
2024-02-05 16:43:35 -0600

    Add test for associations for Merchant

---
+++ spec/models/merchant_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe Merchant, type: :model do
+  describe "associations" do
+    it { should have_many(:invoices)}
+    it { should have_many(:items) }
+    # it { should have_many(transactions).through :invoices }
+    # it { should have_many(invoice_items).through :invoices }
+  end
+end
