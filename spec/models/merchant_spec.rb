3e3e75c915eaf9a7b8a651ea003af12586618d26
Nathan Lambertson <lambertones@gmail.com>
2024-02-05 18:50:32 -0600

    Add validations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Merchant, type: :model do
   describe "associations" do
     it { should have_many(:invoices)}
     it { should have_many(:items) }
-    # it { should have_many(transactions).through :invoices }
-    # it { should have_many(invoice_items).through :invoices }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
   end
 end
