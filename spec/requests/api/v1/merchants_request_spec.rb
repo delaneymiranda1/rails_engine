831356abb82283ef099aa00412add3c4d7ceb1f8
Nathan Lambertson <lambertones@gmail.com>
2024-02-06 17:07:34 -0600

    Add merchants API test

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "merchants API" do
+  it "fetches all merchants" do
+    merchant = create(:merchant)
+
+    get "/api/v1/merchants"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(String)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
+  end
+end
