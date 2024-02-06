3d61670f415cc2c59c887df3ddf934ad34abaa3b
Nathan Lambertson <lambertones@gmail.com>
2024-02-05 19:32:40 -0600

    Add test for customer factory

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -10,4 +10,11 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
   end
+
+  it "can have a full name" do
+    customer = create(:customer)
+
+    expect(customer.first_name).to be_a String
+    expect(customer.last_name).to be_a String 
+  end
 end
