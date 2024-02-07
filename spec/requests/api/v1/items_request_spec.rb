8beea4226d73bbb588f7e2e75a101f609c8203b2
Nathan Lambertson <lambertones@gmail.com>
2024-02-07 15:58:11 -0600

    Add test for get items for a merchant

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -194,4 +194,43 @@ describe "items API", type: :request do
       expect { Item.find(@item2.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
+
+  describe "GET /api/v1/merchants/id/items" do
+    it "happy: fetches all items for a merchant" do
+      merchant = create(:merchant)
+      item1 = create(:item, merchant: merchant)
+      item2 = create(:item, merchant: merchant)
+      item3 = create(:item, merchant: merchant)
+
+      get "/api/v1/merchants/#{merchant.id}/items"
+
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_an(String)
+
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      end
+    end
+
+    it "sad: returns 404 if merchant not found" do
+      get "/api/v1/merchants/7/items"
+
+      expect(response).not_to be_successful
+      expect(response.status).to eq(404)
+    end
+  end
 end
