132726d41e4c6824335612240afb9998dae9fe1a
Miranda Delaney <delaney.miranda1@gmail.com>
2024-02-06 17:24:27 -0700

    Add show method for items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,26 @@
 class Api::V1::ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :validation_error_response
+
   def index
     render json: ItemSerializer.new(Item.all)
   end
+
+  def show  
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item)
+  end
+
+
+  private 
+  
+  def not_found_response(exception) 
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
+  end
+
+  def validation_error_response(exception) 
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400))
+    .serialize_json, status: :bad_request
+  end
+
 end
