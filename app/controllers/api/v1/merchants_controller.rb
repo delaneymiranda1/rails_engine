ba70ac89faad0a8897031073c4b30d18f5c8593c
Miranda Delaney <delaney.miranda1@gmail.com>
2024-02-06 17:24:20 -0700

    Add show method for merchants

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,25 @@
 class Api::V1::MerchantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :validation_error_response
+  
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  private 
+  
+  def not_found_response(exception) 
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404)).serialize_json, status: :not_found
+  end
+
+  def validation_error_response(exception) 
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 400))
+    .serialize_json, status: :bad_request
+  end
+
 end
