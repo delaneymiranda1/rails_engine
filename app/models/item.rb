30a69509f7f00e6ecefa42cd1f89fe6f89500d25
Nathan Lambertson <lambertones@gmail.com>
2024-02-07 09:23:22 -0600

    Add dependencies for destroy  item, but not sure if they are necessary in tandem with controller private method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :merchant
-  has_many :invoice_items
-  has_many :invoices, through: :invoice_items
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
   validates :unit_price, presence: true
+
+  before_destroy :check_for_invoices
+
+  private
+
+  def check_for_invoices# not sure if still needed after adding controller private method, might be useful?
+    invoices.each do |invoice|
+      invoice.destroy if invoice.items.count == 1
+    end
+  end
 end
