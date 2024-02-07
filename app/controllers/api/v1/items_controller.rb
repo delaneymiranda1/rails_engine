1519863d55298d25afdad28aac958a0412c6480e
Nathan Lambertson <lambertones@gmail.com>
2024-02-07 16:28:59 -0600

    Add a bunch more logic to controller for index action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,11 +5,16 @@ class Api::V1::ItemsController < ApplicationController
   def index
     if params[:merchant_id].present?
       merchant = Merchant.find(params[:merchant_id])
-      items = merchant.items
+      if merchant
+        items = merchant.items
+        render json: ItemSerializer.new(items)
+      else
+        render json: { error: 'Merchant not found' }, status: :not_found
+      end
     else
       items = Item.all
+      render json: ItemSerializer.new(items)
     end
-    render json: ItemSerializer.new(items)
   end
 
   def show
